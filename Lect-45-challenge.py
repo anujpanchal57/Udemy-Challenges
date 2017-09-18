a7e22cf11405b0e8c268ade9ce8e386c496d4115
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-18 13:02:57 +0530

    Add files via upload

---
+++ Lect-45-challenge.py
@@ -0,0 +1,11 @@
+# Experiment with different ranges and slices to get a feel for how they work.
+# Remember that you can print the range as well as iterating through it to print
+# its values, to check that your ranges are what you expected.
+# You may also want to include things like.
+#
+o = range(0, 100, 4)
+print(o)
+p = o[::5]
+print(p)
+for i in p:
+    print(i)
